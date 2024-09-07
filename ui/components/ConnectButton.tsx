d2cd884b58371988b63e5136edd21b8ff615ad65
vinhtrand8 <vinhtrand8@gmail.com>
2024-09-07 21:25:58 +0700

    wip: fetch and write to chain

--- ui/components/ConnectButton.tsx
+++ ui/components/ConnectButton.tsx
@@ -3,9 +3,208 @@
 import { useWallet } from '@tronweb3/tronwallet-adapter-react-hooks';
 import { useWalletModal } from '@tronweb3/tronwallet-adapter-react-ui';
 
+const abi = [
+  {
+    inputs: [],
+    name: 'AlreadyExists',
+    type: 'error',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'bytes32',
+        name: 'uid',
+        type: 'bytes32',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'registerer',
+        type: 'address',
+      },
+      {
+        components: [
+          {
+            internalType: 'bytes32',
+            name: 'uid',
+            type: 'bytes32',
+          },
+          {
+            internalType: 'contract ISchemaResolver',
+            name: 'resolver',
+            type: 'address',
+          },
+          {
+            internalType: 'bool',
+            name: 'revocable',
+            type: 'bool',
+          },
+          {
+            components: [
+              {
+                internalType: 'string',
+                name: 'fieldType',
+                type: 'string',
+              },
+              {
+                internalType: 'string',
+                name: 'fieldName',
+                type: 'string',
+              },
+              {
+                internalType: 'string',
+                name: 'fieldDescription',
+                type: 'string',
+              },
+            ],
+            internalType: 'struct SchemaField[]',
+            name: 'schema',
+            type: 'tuple[]',
+          },
+        ],
+        indexed: false,
+        internalType: 'struct SchemaRecord',
+        name: 'schema',
+        type: 'tuple',
+      },
+    ],
+    name: 'Registered',
+    type: 'event',
+    stateMutability: 'nonpayable',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'bytes32',
+        name: 'uid',
+        type: 'bytes32',
+      },
+    ],
+    name: 'getSchema',
+    outputs: [
+      {
+        components: [
+          {
+            internalType: 'bytes32',
+            name: 'uid',
+            type: 'bytes32',
+          },
+          {
+            internalType: 'contract ISchemaResolver',
+            name: 'resolver',
+            type: 'address',
+          },
+          {
+            internalType: 'bool',
+            name: 'revocable',
+            type: 'bool',
+          },
+          {
+            components: [
+              {
+                internalType: 'string',
+                name: 'fieldType',
+                type: 'string',
+              },
+              {
+                internalType: 'string',
+                name: 'fieldName',
+                type: 'string',
+              },
+              {
+                internalType: 'string',
+                name: 'fieldDescription',
+                type: 'string',
+              },
+            ],
+            internalType: 'struct SchemaField[]',
+            name: 'schema',
+            type: 'tuple[]',
+          },
+        ],
+        internalType: 'struct SchemaRecord',
+        name: '',
+        type: 'tuple',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: 'string',
+            name: 'fieldType',
+            type: 'string',
+          },
+          {
+            internalType: 'string',
+            name: 'fieldName',
+            type: 'string',
+          },
+          {
+            internalType: 'string',
+            name: 'fieldDescription',
+            type: 'string',
+          },
+        ],
+        internalType: 'struct SchemaField[]',
+        name: 'schema',
+        type: 'tuple[]',
+      },
+      {
+        internalType: 'contract ISchemaResolver',
+        name: 'resolver',
+        type: 'address',
+      },
+      {
+        internalType: 'bool',
+        name: 'revocable',
+        type: 'bool',
+      },
+    ],
+    name: 'register',
+    outputs: [
+      {
+        internalType: 'bytes32',
+        name: '',
+        type: 'bytes32',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
+
 export const ConnectButton: IComponent = () => {
   const { address, connected, wallet, disconnect } = useWallet();
   const { setVisible } = useWalletModal();
+
+  const handleRegister = async () => {
+    if (connected && window.tronWeb) {
+      console.log('called');
+      const events = await window.tronWeb.event.getEventsByTransactionID(
+        '97039890e29f2b1dad110e283a59c39c712a1ca902ca687ae29c6b0f1580fbd2',
+        { only_confirmed: true }
+      );
+      console.log(events);
+
+      const contract = await (window.tronWeb as any).contract(
+        abi,
+        'TG7ZW9xPDU4FoHdJJfQgs9zoX72ThukLZz'
+      );
+      const data = await contract
+        .getSchema('0x2b4fb0af6589cfbdc945e6a9b293f83be66b5d01b19bb9f37b75bb5564c8c164')
+        .call();
+      console.log(data);
+      const tx = await contract.register([], 'TLY4qnH7TaLRVrmXdwFKUzs2BR9pxuTPXZ', false).send();
+      console.log(tx);
+    }
+  };
 
   return (
     <div>
@@ -20,6 +219,9 @@ export const ConnectButton: IComponent = () => {
               <span>Your Address:</span> {address}
             </p>
             <button onClick={disconnect}>Disconnect Wallet</button>
+          </div>
+          <div>
+            <button onClick={handleRegister}>Register</button>
           </div>
         </div>
       ) : (
