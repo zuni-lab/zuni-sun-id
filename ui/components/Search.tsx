f801f3fba8ad44d3f2782459639eb9cb058e54af
David Tran <tranhuyducseven@gmail.com>
2024-09-22 10:45:04 +0700

    feat: init search Modal

--- ui/components/Search.tsx
+++ ui/components/Search.tsx
@@ -1,5 +1,5 @@
 import { useActionDebounce } from '@/hooks/useAction';
-import { SearchIcon, Slash } from 'lucide-react';
+import { Loader, SearchIcon, Slash } from 'lucide-react';
 import { useCallback, useState } from 'react';
 import { Button } from './shadcn/Button';
 import { Input } from './shadcn/Input';
@@ -46,31 +46,45 @@ export const Search: IComponent<{
         createdAt: response.created_at,
       };
 
-      console.log({ parsedData });
+      return parsedData;
     },
     enabled: !!query,
   });
 
-  console.log(data, isFetching);
-
   return (
-    <div className="w-full flex items-center float-end bg-accent px-2 rounded-2xl">
-      <Button size={'lg'} variant={'link'} className="px-2 cursor-default">
-        <SearchIcon className="w-5 h-5" />
-      </Button>
-      <Input
-        placeholder={placeholder}
-        className="focus:!border-none bg-transparent placeholder:text-gray-500"
-        onChange={onSearch}
-        // value={query}
-      />
-      <Button
-        size={'lg'}
-        variant={'link'}
-        className="h-fit w-fit mr-2 px-1 py-2 cursor-pointer border border-muted-foreground hover:text-gray-500 hover:border-muted-foreground"
-        onClick={clearSearch}>
-        <Slash className="w-4 h-4" />
-      </Button>
+    <div className="flex flex-col divide-y divide-white max-w-4xl">
+      <div className="w-full flex items-center float-end bg-accent px-2 rounded-2xl">
+        <Button size={'lg'} variant={'link'} className="px-2 cursor-default">
+          <SearchIcon className="w-5 h-5" />
+        </Button>
+        <Input
+          placeholder={placeholder}
+          className="focus:!border-none bg-transparent placeholder:text-gray-500"
+          onChange={onSearch}
+          // value={query}
+        />
+        <Button
+          size={'lg'}
+          variant={'link'}
+          className="h-fit w-fit mr-2 px-1 py-2 cursor-pointer border border-muted-foreground hover:text-gray-500 hover:border-muted-foreground"
+          onClick={clearSearch}>
+          <Slash className="w-4 h-4" />
+        </Button>
+      </div>
+      {isFetching && (
+        <div className="bg-accent px-2 mt-1 border-t border-white rounded-xl min-h-32">
+          <div className="h-full w-full flex items-center justify-center py-8">
+            <Loader className="w-10 h-10 animate-spin" />
+          </div>
+        </div>
+      )}
+      {data && (
+        <div className="bg-accent px-2 mt-1 border-t border-white rounded-xl min-h-32 max-w-full">
+          <div className="h-full w-full flex items-center justify-center py-8">
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
